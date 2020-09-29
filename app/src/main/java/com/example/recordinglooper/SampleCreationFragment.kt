3e2595591d3f79baf73893fe3cdf3ead5264f752
Harry Alexander <harialexandre@gmail.com>
2020-09-28 23:40:44 -0400

    Partial SampleCreationFragment implementation

--- app/src/main/java/com/example/recordinglooper/SampleCreationFragment.kt
+++ app/src/main/java/com/example/recordinglooper/SampleCreationFragment.kt
@@ -1,6 +1,62 @@
 package com.example.recordinglooper
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.MediaRecorder
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.visualizer.amplitude.AudioRecordView
+import java.io.IOException
 
 class SampleCreationFragment : Fragment() {
+
+    private lateinit var audioVisualisation: AudioRecordView
+    private lateinit var mediaRecorder: MediaRecorder
+    private lateinit var recordButton: Button
+    private var isRecordingPermitted = false
+    private var permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        isRecordingPermitted = if (requestCode == 200) {
+            grantResults[0] == PackageManager.PERMISSION_GRANTED
+        } else {
+            false
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mediaRecorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.DEFAULT)
+            setOutputFile("")
+            setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT)
+            try {
+                prepare()
+            } catch (e: IOException) {
+                Log.e("RECORDER_INIT", "Failed to prepare")
+            }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_sample_creation, container, false)
+        audioVisualisation = view.findViewById(R.id.audo_visualisation)
+        recordButton = view.findViewById(R.id.record_button)
+        return view
+    }
 }
